1ecfc4ce2ed9bf7c7e4c9b3c53c586481831a357
root <root@ip-10-42-189-144.ec2.internal>
2026-04-03 18:08:41 +0000

    add part4 and the files

---
+++ part4/frontend/scripts.js
@@ -0,0 +1,40 @@
+const API_URL = "http://127.0.0.1:5000";
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';')[0];
+    return null;
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+
+            try {
+                const response = await fetch(`${API_URL}/auth/login`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ email, password })
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    document.cookie = `token=${data.access_token}; path=/`;
+                    window.location.href = 'index.html';
+                } else {
+                    alert('Login failed: Invalid credentials');
+                }
+            } catch (error) {
+                alert('Error connecting to server');
+            }
+        });
+    }
+});
