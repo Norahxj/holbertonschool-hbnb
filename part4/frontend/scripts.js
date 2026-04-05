d7849a0b5e8ecb7a84d181779fe63a70a4525a27
manaralthqfiu <9vieo677@gmail.com>
2026-04-05 08:38:25 +0000

    add scripts.js

--- part4/frontend/scripts.js
+++ part4/frontend/scripts.js
@@ -1,12 +1,3 @@
-const API_URL = "http://127.0.0.1:5000/api/v1";  // BASE URL
-
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';')[0];
-    return null;
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
 
@@ -18,10 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
             const password = document.getElementById('password').value;
 
             try {
-                const response = await fetch(`${API_URL}/auth/login`, {
-                    method: 'POST',
+                const response = await fetch("http://127.0.0.1:5000/api/v1/auth/login", {
+                    method: "POST",
                     headers: {
-                        'Content-Type': 'application/json'
+                        "Content-Type": "application/json"
                     },
                     body: JSON.stringify({ email, password })
                 });
@@ -29,12 +20,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (response.ok) {
                     const data = await response.json();
                     document.cookie = `token=${data.access_token}; path=/`;
-                    window.location.href = 'index.html';
+                    window.location.href = "index.html";
                 } else {
-                    alert('Login failed: Invalid credentials');
+                    alert("Login failed");
                 }
             } catch (error) {
-                alert('Error connecting to server');
+                alert("Error connecting to server");
             }
         });
     }
