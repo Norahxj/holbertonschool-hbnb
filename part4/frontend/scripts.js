2f9383aa391d4d488b3b18af553a495a33bdaeb2
manaralthqfiu <9vieo677@gmail.com>
2026-04-05 14:32:09 +0000

    fix the path at scripts.js file

--- part4/frontend/scripts.js
+++ part4/frontend/scripts.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const password = document.getElementById("password").value;
 
             try {
-                const response = await fetch("http://127.0.0.1:5000/api/v1/auth", {
+                const response = await fetch("http://127.0.0.1:5000/api/v1/auth/login", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
