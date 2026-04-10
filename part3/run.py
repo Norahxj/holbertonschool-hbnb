f3a7ebcbc627f9bed04e070b7fc932e9e4638882
Norahx <Nhaljuhani@gmail.com>
2026-04-10 13:16:12 +0000

    Amendment to run.py

--- part3/run.py
+++ part3/run.py
@@ -1,9 +1,19 @@
+from flask import send_from_directory
 from app import create_app, db
-
+import os
 
 app = create_app()
+
 with app.app_context():
     db.create_all()
 
+FRONTEND_FOLDER = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..', 'part4')
+)
+
+@app.route('/<path:path>')
+def serve_frontend(path):
+    return send_from_directory(FRONTEND_FOLDER, path)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
